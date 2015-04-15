391e4fc9ff8dda76928c52bd2ea79aa1b2e44d4d
Paul <pwittschen@gmail.com>
2015-04-15 11:53:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -47,6 +47,10 @@ $(function() {
 			},
 			error: function(xhr) {
 				console.log('AJAX ERROR: ' + xhr.responseText);
+				$("#forecast_container").html("<p class='error_message'>There's an issue getting the weather. <a id='reload'>Try again</a></p>");
+				$("#reload").on('click', function() {
+					document.location.reload(true);
+				});
 			}
 		});
 	}
@@ -71,5 +75,46 @@ $(function() {
 		});
 		// instantiate forecast view
 		var weather_forecast_view = new Forecast_View({ model: data});
+		responsive_box_height();
 	}
+
+
+
+
+	/* ==================== */
+	/* Misc. Event Handlers */
+
+	/* animate extra weather info */
+	$(window).resize(function() {
+		responsive_box_height();
+	});
+	function responsive_box_height() {
+		if ( ($(window).width()) < 1160 ) {
+			$('.weather_item').addClass('mobile');
+		} else {
+			$('.weather_item').removeClass('mobile')
+		}
+	}
+
+	$('body').on('click', '.weather_item', function() {
+
+		var el = $(this);
+
+		// hide/animate up
+		if (el.hasClass('reveal')) {
+			if (el.hasClass('mobile')) {
+				el.animate({'height':'125px'}, 700);
+			}
+			el.children('.weather_extra').animate({'top':'0'}, 400);
+
+		// reveal/animate down
+		} else {
+			if (el.hasClass('mobile')) {
+				el.animate({'height':'250px'}, 400);
+			}
+			el.children('.weather_extra').css('display','inline').animate({'top':'125px'}, 700);
+		}
+		el.toggleClass('reveal');
+	});
 });
+
