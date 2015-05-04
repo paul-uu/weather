a81e1e22e5059d2b80bd74bb561a778a051ad43e
Paul <pwittschen@gmail.com>
2015-05-03 22:04:24 -0400

    Update main.js
    Instead of passing JSON object directly to a Backbone View, the JSON is passed into a collection view, which passes the data into a Collection (which is configured to a Backbone model) for facilitating future modifications. Each model in this collection is then instantiated as a backbone view, nested inside a collection view.
    
    Events on each 'model' view (individual weather item) has also been re-implemented into the backbone view definition (instead of plain jQuery).

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var app = app || {},
+	day = new Date(), 
+	days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],	
+	month = ['January','February','March','April','May','June','July','August','Spetember','October','November'];
+
 
 $(function() {
 
@@ -59,26 +64,110 @@ $(function() {
 	/* Render weather data using a backbone view */
 	function go_backbone(data) {
 
-		var Forecast_View = Backbone.View.extend({
-			el: $('#forecast_container'),
-			template: _.template( $('#forecast_template').html() ),
+		// Model definition
+		app.Weather_Item = Backbone.Model.extend({
+		});
+		// Collection definition
+		app.Forecast = Backbone.Collection.extend({
+			model: app.Weather_Item
+		});
+
+
+		// View for individual weather item
+		app.Weather_View = Backbone.View.extend({
+			tagName: 'div',
+			className: 'weather_item',
+			template: _.template( $('#weather_template').html() ),
+			events: {
+				'click':'show_hide'
+			},
 			initialize: function() {
 				this.render();
 			},
-			render: function() {
-				console.log(this.model);
-				this.$el.html( this.template(this.model) );
-				return this;
+
+			// the index is passed from app.Forecast_View.renderItem
+			// and represents the index of the current model of the 
+			// collection
+			render: function(index) {
+				if (index >= 0) {
+					var date_html = return_date(index);
+					this.$el.html( this.template(this.model) );
+					this.$el.find('.weather_main').prepend(date_html);
+					return this;
+				}
+			},
+
+			// Events for weather view
+			show_hide: function() {
+				var weather_el = this.$el;
+				if (weather_el.hasClass('reveal')) {
+					if (weather_el.hasClass('mobile')) {
+						weather_el.animate({'height':'125px'}, 700);
+					}
+					weather_el.children('.weather_extra').animate({'top':'0'}, 400);
+				} else {
+					if (weather_el.hasClass('mobile')) {
+						weather_el.animate({'height':'250px'});
+					}
+					weather_el.children('.weather_extra').animate({'top':'125px'}, 700);
+				}
+				weather_el.toggleClass('reveal');
 			}
 		});
+
+		// View for weather collection (forecast)
+		app.Forecast_View = Backbone.View.extend({
+			el: '#forecast_container',
+			template: _.template( $('#forecast_template').html() ),
+			initialize: function(data) {
+				
+				this.collection = new app.Forecast(data.list);
+				var city_name = data.city.name;
+				this.render(city_name);
+			},
+			render: function(city_name) {
+
+				this.$el.empty();
+
+				// place city_name into an object for the collection
+				// template to read from -- simply passing the template
+				// the city_name as a string will not work; expects obj
+				var city_obj = {
+					"city":city_name
+				};
+
+				// underscore each function
+				// function is the iteratee function
+				// second argument this is the context object
+				this.collection.each( function(model, i) {
+					this.renderItem(model, i);
+				}, this);
+
+				this.$el.prepend( this.template(city_obj) );
+				return this;
+			},
+			renderItem: function(model, i) {
+
+				var modelView = new app.Weather_View({
+					model: model
+				});
+				this.$el.append( modelView.render(i).el );
+			}
+		});
+
+
 		// instantiate forecast view
-		var weather_forecast_view = new Forecast_View({ model: data});
+		app.forecast_view = new app.Forecast_View(data);
+
+		// add '.mobile' class to '.weather_item's if appropriate
 		responsive_box_height();
 	}
 
 
-	/* ==================== */
-	/* Misc. Event Handlers */
+
+
+	/* =============== */
+	/* Misc. Functions */
 
 	// add class 'mobile' to .weather_item elements
 	$(window).resize(function() {
@@ -92,15 +181,27 @@ $(function() {
 		}
 	}
 
+	// determine date for current model within a collection,
+	// where i represents the index value of the model.
+	// returns html w/ correct date for app.Weather_View
+	function return_date(i) {
+		if (i === 0) {
+			day.setDate(day.getDate());
+		} else {
+			day.setDate(day.getDate() + 1);
+		}
+		return "<div class='weather_date' style='float:left;'><span>" + month[day.getMonth()] + " " + day.getDate() + "</span><br><span>" + days[day.getDay()] + "</span></div>";
+	}
+
+
 	/* animate extra weather info */
-	$('body').on('click', '.weather_item', function() {
-		var el = $(this);
-		toggle_weather(el);
+	$('body').on('click', '#hide_all', function() {
+		hide_all();
 	});
-	$('body').on('click', '#hide_all', hide_all() );
-	$('body').on('click', '#show_all', show_all() );
-	
-	
+	$('body').on('click', '#show_all', function() {
+		show_all();
+	});
+
 	function toggle_weather(element) {
 		if (element.hasClass('reveal')) {
 			if (element.hasClass('mobile')) {
